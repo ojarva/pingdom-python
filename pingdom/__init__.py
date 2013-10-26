d6200362bb609a40ee4feb5af12f00081f2e3b83
Glenn Snyder <gsnyder2007@gmail.com>
2013-10-26 09:24:46 -0400

    Added support for creating, modifying, and deleting Pingdom contacts.

--- pingdom/__init__.py
+++ pingdom/__init__.py
@@ -4,12 +4,33 @@
     pingdom.__init__
 
 """
+from pkg_resources import get_distribution, DistributionNotFound
+
+#
+# Added support for surfacing the VERSION, __version__ attributes
+# with the package installation.
+#
+# Ref: http://stackoverflow.com/questions/17583443/what-is-the-correct-way-to-share-package-version-with-setup-py-and-the-package
+#
+__project__ = 'pingdom'
+__version__ = None  # required for initial installation
 
 __author__ = 'Mike Babineau <michael.babineau@gmail.com>'
 __copyright__ = "Copyright 2011 Electronic Arts Inc."
 __license__ = "Apache v2.0"
 
+try:
+    __version__ = get_distribution(__project__).version
+except DistributionNotFound:
+    VERSION = __project__ + '-' + '(local)'
+else:
+    VERSION = __project__ + '-' + __version__
+
 from pingdom.connection import (PingdomRequest, PingdomResponse,
                                  PingdomConnection)
 from pingdom.exception import PingdomError
 from pingdom.resources import PingdomCheck, PingdomContact
+
+
+
+
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='pingdom',
-      version="0.2.0",
+      version="0.3.0",
       description='Pingdom Library',
       long_description="""3rd-party Python interface to Pingdom's REST API.""",
       author='Mike Babineau',
